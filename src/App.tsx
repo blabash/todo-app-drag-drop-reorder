81ce979af71c54495ce7d01bec08491e7cfa5d69
Brett Labash <brett.labash@gmail.com>
2022-06-09 12:43:25 -0700

    Add static styling to light mode checkmarks

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import logo from "./logo.svg";
 import moonIcon from "./images/icon-moon.svg";
+import checkIcon from "./images/icon-check.svg";
+import crossIcon from "./images/icon-cross.svg";
 import "./App.css";
 
 function App() {
@@ -21,21 +23,47 @@ function App() {
           placeholder="Create a new todo..."
         />
         <ul>
-          <li className="padding-1em">test 1</li>
-          <li className="padding-1em">test 2</li>
-          <li className="padding-1em">test 3</li>
-          <li className="padding-1em">test 4</li>
-          <li className="flex-row justify-space-between padding-1em">
+          <li className="todo-item padding-1em">
+            <span className="check-icon-bg flex-row justify-content-center">
+              <img src={checkIcon} alt="moon icon" />
+            </span>
+            <p>test 1</p>
+            <img src={crossIcon} alt="moon icon" />
+          </li>
+          <li className="todo-item padding-1em">
+            <span className="check-icon-bg flex-row justify-content-center">
+              <img src={checkIcon} alt="moon icon" />
+            </span>
+            <p>test 1</p>
+            <img src={crossIcon} alt="moon icon" />
+          </li>
+          <li className="todo-item padding-1em">
+            <span className="check-icon-bg flex-row justify-content-center">
+              <img src={checkIcon} alt="moon icon" />
+            </span>
+            <p>test 1</p>
+            <img src={crossIcon} alt="moon icon" />
+          </li>
+          <li className="todo-item padding-1em">
+            <span className="check-icon-bg flex-row justify-content-center">
+              <img src={checkIcon} alt="moon icon" />
+            </span>
+            <p>test 1</p>
+            <img src={crossIcon} alt="moon icon" />
+          </li>
+          <li className="flex-row justify-space-between padding-1em dark-grayish-blue">
             <p>5 items left</p>
             <button>Clear Completed</button>
           </li>
         </ul>
-        <div className="flex-row justify-content-center padding-1em">
+        <div className="flex-row justify-content-center padding-1em font-weight-bold">
           <button>All</button>
           <button>Active</button>
           <button>Completed</button>
         </div>
-        <p className="center-text">Drag and drop to reorder list</p>
+        <p className="center-text dark-grayish-blue margin-top-1em">
+          Drag and drop to reorder list
+        </p>
       </div>
     </main>
   );
