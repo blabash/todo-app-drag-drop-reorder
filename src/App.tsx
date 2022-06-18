5d0243dc615303867abb8b5aabcf038cddebc4f0
Brett Labash <brett.labash@gmail.com>
2022-06-17 22:13:10 -0700

    Remove commented code that turned into Todos component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useReducer, useState } from "react";
-import logo from "./logo.svg";
 import moonIcon from "./images/icon-moon.svg";
 import checkIcon from "./images/icon-check.svg";
 import crossIcon from "./images/icon-cross.svg";
@@ -177,57 +176,6 @@ function App() {
           </button>
         </div>
         <Todos />
-        {/* <div className="todos-container__todo-input-container">
-          <span className="todos-container__checkbox">
-            <img src={checkIcon} alt="check icon" />
-          </span>
-          <input
-            className="todos-container__todo-input"
-            type="text"
-            name=""
-            id=""
-            placeholder="Create a new todo..."
-          />
-        </div> */}
-        {/* <ul className="todos-container__todo-list">
-          <li className="todos-container__todo-item">
-            <span className="todos-container__checkbox todos-container__checkbox--complete">
-              <img src={checkIcon} alt="check icon" />
-            </span>
-            <p>test 1</p>
-            <img src={crossIcon} alt="cross icon" />
-          </li>
-          <li className="todos-container__todo-item">
-            <span className="todos-container__checkbox todos-container__checkbox--complete">
-              <img src={checkIcon} alt="check icon" />
-            </span>
-            <p>test 1</p>
-            <img src={crossIcon} alt="cross icon" />
-          </li>
-          <li className="todos-container__todo-item">
-            <span className="todos-container__checkbox todos-container__checkbox--complete">
-              <img src={checkIcon} alt="check icon" />
-            </span>
-            <p>test 1</p>
-            <img src={crossIcon} alt="cross icon" />
-          </li>
-          <li className="todos-container__todo-item">
-            <span className="todos-container__checkbox todos-container__checkbox--complete">
-              <img src={checkIcon} alt="check icon" />
-            </span>
-            <p>test 1</p>
-            <img src={crossIcon} alt="cross icon" />
-          </li>
-          <li className="todos-container__status-tray">
-            <p>5 items left</p>
-            <div className="todos-container__filters todos-container__filters--desktop">
-              <button>All</button>
-              <button>Active</button>
-              <button>Completed</button>
-            </div>
-            <button>Clear Completed</button>
-          </li>
-        </ul> */}
         <div className="todos-container__filters todos-container__filters--mobile">
           <button>All</button>
           <button>Active</button>
